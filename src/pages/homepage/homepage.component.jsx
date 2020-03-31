20315c077607d783b201af076dc104d543671e31
Christian Halsell <christian.halsell@verys.com>
2020-03-30 18:04:26 -0700

    Style banner and search area

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="banner">
+      <div className="banner-content">
+        <div className="banner-eevee"></div>
+        <div className="banner-logo"></div>
+        <div className="banner-pikachu"></div>
+      </div>
+    </div>
+
+    <div className="search">
+      <div className="search-box">
+        <input
+          className="search-field"
+          type="search"
+          placeholder="Search for Pokemon..."
+        />
+      </div>
+    </div>
+
+    <div className="card-list">
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card-image">
+          image
+        </div>
+        <div className="card-text">
+          text
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
