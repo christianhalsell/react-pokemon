8fbae21e3dd2fbc77c8571fb8b8cf8bb6ae3fe18
Christian Halsell <shinobiwulf@gmail.com>
2020-04-01 17:33:13 -0700

    Updates

---
+++ precache-manifest.a7ffee6d457838dfe507b8c4d6c1ee9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2206f0f253993b389068fdd777e477ea",
+    "url": "/react-pokemon/index.html"
+  },
+  {
+    "revision": "6cfd8499aa0a36a3381b",
+    "url": "/react-pokemon/static/css/main.0132b0b7.chunk.css"
+  },
+  {
+    "revision": "9945f7dd55e7c166ecd8",
+    "url": "/react-pokemon/static/js/2.041e5a47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokemon/static/js/2.041e5a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfd8499aa0a36a3381b",
+    "url": "/react-pokemon/static/js/main.c3dc6d41.chunk.js"
+  },
+  {
+    "revision": "80b706af44bdf0f73c7d",
+    "url": "/react-pokemon/static/js/runtime-main.c4a34489.js"
+  },
+  {
+    "revision": "92be670a0f54870fb134b837ef9b39d4",
+    "url": "/react-pokemon/static/media/eevee.92be670a.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/react-pokemon/static/media/pikachu.76f027c1.png"
+  },
+  {
+    "revision": "ddd5084dff8d7606019645f62c1712c6",
+    "url": "/react-pokemon/static/media/pokemon-logo-png-pokemon-logo-png-2000.ddd5084d.png"
+  }
+]);
