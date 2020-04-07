81127d78e3140bb922c90d43efc3868a3ce3aed4
Christian Halsell <shinobiwulf@gmail.com>
2020-04-07 14:07:34 -0700

    Added routing to Details page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const DetailsPage = ({ match }) => (
+  <div>Details on { match.params.id}</div>
+);
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/details/:id" component={DetailsPage} />
+      </Switch>
     </div>
   );
 }
