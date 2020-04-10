52bcda0eb9c55d82a7dd93566416edc63094f437
Christian Halsell <shinobiwulf@gmail.com>
2020-04-10 15:55:53 -0700

    More stats and alternate image for details

--- src/pages/details/details.component.jsx
+++ src/pages/details/details.component.jsx
@@ -20,27 +20,32 @@ class Details extends React.Component {
 
   render() {
     const pokeDetails = this.state.pokeDetails;
+    let typesArray = pokeDetails.types ? pokeDetails.types.reverse() : null;
+
+    console.log(typesArray)
 
     return (
       <div className="pokemon-details">
         <h1>Details on <span className="pokemon-name">{pokeDetails.name}</span></h1>
 
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeDetails.id}.png`} alt="" />
-        <div>Name: <span className="pokemon-name">{pokeDetails.name}</span></div>
-        <div>Weight: {pokeDetails.weight} lbs</div>
         <div>Background Image: {
           pokeDetails.types ? pokeDetails.types.map((item) => item.slot === 1 ? item.type.name : null) : null
           // TODO: Reverse array so I don't have to nest terinary operators
         }
         </div>
-        <div>All Types:</div>
+        <img className="" src={`https://pokeres.bastionbot.org/images/pokemon/${pokeDetails.id}.png`} alt="" />
+        <div>Name: <span className="pokemon-name">{pokeDetails.name}</span></div>
+        <div>Weight: {Math.round(pokeDetails.weight / 4.536)} lbs</div>
+        <div>Types:</div>
         <div>
           <ul>
             {
-              pokeDetails.types ? pokeDetails.types.map((item, index) => <li key={index}>{item.type.name}</li>) : ''
+              pokeDetails.types ? pokeDetails.types.map((item) => <li key={item.type.name}>{item.type.name}</li>) : ''
             }
           </ul>
         </div>
+        <div>Height: {Math.round(pokeDetails.height * 10 / 2.54)} in</div>
+        <div>Abilities:</div>
       </div>
     )
   }
