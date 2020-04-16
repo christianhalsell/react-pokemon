77dc9a487ac6e15009c360fe907c785691d0678c
Christian Halsell <shinobiwulf@gmail.com>
2020-04-15 20:34:56 -0700

    Pokemon stats details section

--- src/components/pokemon-details/pokemon-details.component.jsx
+++ src/components/pokemon-details/pokemon-details.component.jsx
@@ -30,17 +30,24 @@ const PokemonDetails = ({ id, types, name, weight, height }) => {
         </div>
 
         <div className="pokemon-name">{name}</div>
+
         <div className="pokemon-stats">
-          <div className="pokemon-stats_weight">
-            <div>{Math.round(weight / 4.536)} lb</div>
-            <div>Weight</div>
+          <div className="pokemon-stats__data-row">
+            <div className="stats-weight">{Math.round(weight / 4.536)} lb</div>
+            <div className="stats-type">
+              <div className={`type-icon ${typeMain}`}></div>
+              {typeSecondary ? <div className={`type-icon secondary ${typeSecondary}`}></div> : null}
+            </div>
+            <div className="stats-height">{Math.round(height * 10 / 2.54)} in</div>
           </div>
-          <div className="pokemon-stats_types">
-            {typeMain}{typeSecondary ? ` / ${typeSecondary}` : null} {/* TODO: might be a better way */}
-          </div>
-          <div className="pokemon-stats_height">
-            <div>{Math.round(height * 10 / 2.54)} in</div>
-            <div>Height</div>
+          <div className="pokemon-stats__label-row">
+            <div className="stats-weight">Weight</div>
+            <div className="stats-type">
+              {typeMain}
+              {typeSecondary ? <div className="type-divider"> / </div> : null}
+              {typeSecondary ? typeSecondary : null}
+            </div>
+            <div className="stats-height">Height</div>
           </div>
         </div>
       </div>
