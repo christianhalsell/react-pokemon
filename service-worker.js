8491538be2171e5856f33d39d0fe9c00dfc44b79
Christian Halsell <shinobiwulf@gmail.com>
2020-04-17 11:47:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pokemon/precache-manifest.a0bd1dc665451ab8031ac3de41a3b7c2.js"
+  "/react-pokemon/precache-manifest.2d7eec20d4f4786b19326390b5ff1796.js"
 );
 
 self.addEventListener('message', (event) => {
