eeda88ce95250a2e3eecd8124c27ab2a5c0016bd
Christian Halsell <shinobiwulf@gmail.com>
2020-04-17 17:01:52 -0700

    Updates

---
+++ precache-manifest.d98ef31a3dd98496be51a014d5770b5d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4be77f55df92d82a6197885af010424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3637786b0305d18b4922",
+    "url": "/static/css/main.c7d956ba.chunk.css"
+  },
+  {
+    "revision": "adcb860a4f4b5ee6c9eb",
+    "url": "/static/js/2.6b05e678.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b05e678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3637786b0305d18b4922",
+    "url": "/static/js/main.dd18c2ec.chunk.js"
+  },
+  {
+    "revision": "25e03791049ca1146a05",
+    "url": "/static/js/runtime-main.2d094510.js"
+  },
+  {
+    "revision": "31c4942dd9fec1d7fd52bb0a5932daac",
+    "url": "/static/media/bug.31c4942d.png"
+  },
+  {
+    "revision": "f67ff9eb0923bb3c5c4324e1b0a07f53",
+    "url": "/static/media/dark.f67ff9eb.png"
+  },
+  {
+    "revision": "9d2eac6e0f74a097adeff832355842d4",
+    "url": "/static/media/dragon.9d2eac6e.png"
+  },
+  {
+    "revision": "92be670a0f54870fb134b837ef9b39d4",
+    "url": "/static/media/eevee.92be670a.png"
+  },
+  {
+    "revision": "5646f1f8591faa49bfe6dfbf44c9b195",
+    "url": "/static/media/electric.5646f1f8.png"
+  },
+  {
+    "revision": "13e62b1e5eabad3788c5db3d141493d7",
+    "url": "/static/media/fairy.13e62b1e.png"
+  },
+  {
+    "revision": "53b26215a47d39d4e8fb5ee9af945dca",
+    "url": "/static/media/fighting.53b26215.png"
+  },
+  {
+    "revision": "43299c3d9f849259cebd8ad949a8ece1",
+    "url": "/static/media/fire.43299c3d.png"
+  },
+  {
+    "revision": "5c3641ba9056105c34097f97f3bc3f49",
+    "url": "/static/media/flying.5c3641ba.png"
+  },
+  {
+    "revision": "20958252be39bbafaa56d1140f5fee3a",
+    "url": "/static/media/ghost.20958252.png"
+  },
+  {
+    "revision": "b4d2c69661c2da5729c3e81e9c576ef9",
+    "url": "/static/media/grass.b4d2c696.png"
+  },
+  {
+    "revision": "f13df756cf88e5f265abe843577cfd9e",
+    "url": "/static/media/ground.f13df756.png"
+  },
+  {
+    "revision": "ddff30519562115e34950cd2726e03d3",
+    "url": "/static/media/ice.ddff3051.png"
+  },
+  {
+    "revision": "b5ce7f1c1f5c972e4e69ea30c06c3aaf",
+    "url": "/static/media/normal.b5ce7f1c.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/static/media/pikachu.76f027c1.png"
+  },
+  {
+    "revision": "5d6a3faf7058b776d45c4aa4d05ebdf3",
+    "url": "/static/media/poison.5d6a3faf.png"
+  },
+  {
+    "revision": "ddd5084dff8d7606019645f62c1712c6",
+    "url": "/static/media/pokemon-logo-png-pokemon-logo-png-2000.ddd5084d.png"
+  },
+  {
+    "revision": "faaab9a9b280a3774f76b0785303f4d7",
+    "url": "/static/media/psychic.faaab9a9.png"
+  },
+  {
+    "revision": "0e992d09e543a1308a454df2b59ed7c2",
+    "url": "/static/media/rock.0e992d09.png"
+  },
+  {
+    "revision": "0d16504a5d70f25ed00a993431e5e084",
+    "url": "/static/media/steel.0d16504a.png"
+  },
+  {
+    "revision": "0dc637d1c4afee15ca3aa16ad374ccec",
+    "url": "/static/media/water.0dc637d1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pokemon/precache-manifest.1952de3001c348437b32962323342f48.js"
+  "/precache-manifest.d98ef31a3dd98496be51a014d5770b5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pokemon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
