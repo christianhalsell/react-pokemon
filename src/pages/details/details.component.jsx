02087faf6fd2ab900123dceb79ba62fa7cf8a06e
Christian Halsell <shinobiwulf@gmail.com>
2020-04-18 15:02:03 -0700

    Cleaned up commented-out code

--- src/pages/details/details.component.jsx
+++ src/pages/details/details.component.jsx
@@ -27,35 +27,6 @@ class Details extends React.Component {
     return (
       <div className="pokemon-details">
         {pokemonDetails.id ? <PokemonDetails {...pokemonDetails} /> : <Loading />}
-        { /*
-        <div className="pokemon-details-wrapper grass">
-          <div className="pokemon-details-container">
-            <div className="pokemon-details-container__banner">
-              <BackButton />
-              <div>Background Image: {
-                pokemonDetails.types ? pokemonDetails.types.map((item) => item.slot === 1 ? item.type.name : null) : null
-                // TODO: Reverse array so I don't have to nest terinary operators
-              }
-              </div>
-            </div>
-
-            <div className="pokemon-image">
-              <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemonDetails.id}.png`} alt="" />
-            </div>
-
-            <div><span className="pokemon-name">{pokemonDetails.name}</span></div>
-            <div>
-              <div>Weight: {Math.round(pokemonDetails.weight / 4.536)} lbs</div>
-              <div>
-                {
-                  pokemonDetails.types ? pokemonDetails.types.map((item) => <span key={item.type.name}>{item.type.name}</span>) : ''
-                }
-              </div>
-              <div>Height: {Math.round(pokemonDetails.height * 10 / 2.54)} in</div>
-            </div>
-          </div>
-        </div>
-        */}
       </div>
     )
   }
