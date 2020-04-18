28006f919e756b9d62e74c334f8cea9e6f0d33fb
Christian Halsell <shinobiwulf@gmail.com>
2020-04-18 10:24:44 -0700

    Added basename to BrowserRouter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pokédex
 Search for your favorite Pokémon. Created in React.
+
+You can view the live site here: https://christianhalsell.github.io/react-pokemon/
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={'/react-pokemon'}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
