b62c69a9d8a3b98e7e295656ab071b9ff094193a
Christian Halsell <shinobiwulf@gmail.com>
2020-04-18 13:43:04 -0700

    Back button design

--- src/components/back-button/back-button.component.jsx
+++ src/components/back-button/back-button.component.jsx
@@ -3,7 +3,9 @@ import { withRouter } from 'react-router-dom';
 import './back-button.styles.scss';
 
 const BackButton = ({ history }) => (
-  <button className="back-button" onClick={() => history.push('/')}>Back to Homepage</button>
+  <button className="back-button" onClick={() => history.push('/')}>
+    <span>Back to Homepage</span>
+  </button>
 );
 
 export default withRouter(BackButton);
