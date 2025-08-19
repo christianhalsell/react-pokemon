178e06d47cdcda3986da9d41e0b3fcf4b178e550
Christian Halsell <shinobiwulf@gmail.com>
2025-08-18 20:51:24 -0700

    added large images

--- src/components/image-large/image-large.component.jsx
+++ src/components/image-large/image-large.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './image-large.styles.scss';
 
 const PokemonImageLarge = ({ id, name }) => (
-  <div className="pokemon-image">
-    <img src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} alt={name} />
+  <div className='pokemon-image'>
+    {/* <img src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} alt={name} /> */}
+    <img
+      src={`${window.location.origin}/react-pokemon/images/large/${id}-lg.png`}
+      alt={name}
+    />
   </div>
 );
 
